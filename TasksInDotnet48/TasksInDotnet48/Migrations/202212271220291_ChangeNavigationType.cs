abd4ae3d5d648bcadada60d507dc6e464b05f955
niman121 <121780634+niman121@users.noreply.github.com>
2023-01-02 12:32:27 +0530

    Add files via upload

---
+++ TasksInDotnet48/TasksInDotnet48/Migrations/202212271220291_ChangeNavigationType.cs
@@ -0,0 +1,16 @@
+﻿namespace TasksInDotnet48.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeNavigationType : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}

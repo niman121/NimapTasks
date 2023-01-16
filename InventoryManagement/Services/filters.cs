49ec6415253715ca60f44b4613df3dc476c9dc13
niman121 <121780634+niman121@users.noreply.github.com>
2023-01-16 18:35:58 +0530

    Add files via upload

---
+++ InventoryManagement/Services/filters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagement.Services
+{
+    public class CustomFilter : ActionFilterAttribute  
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.Controller.ViewBag.Result = "Filter on Result Executing";
+            base.OnResultExecuting(context);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.Controller.ViewBag.Action = "Filter on Action Executed";
+            base.OnActionExecuting(context);
+        }
+    }
+
+    public class CustomExceptionFilter : FilterAttribute,IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            filterContext.Controller.ViewBag.Exception = "EROROROROROROROROROROROROROARARARARARARARRARARARROROROROROROR";
+            filterContext.Exception.Message.Insert(1,"EROROROROROROROROROROROROROARARARARARARARRARARARROROROROROROR");
+            
+
+        }
+    }
+}
---
+++ InventoryManagement/ViewModels/AccountViewModels.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+namespace InventoryManagement.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "user name is mandatory")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "user name is mandatory")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "email is required")]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [System.Web.Mvc.Compare("Password",ErrorMessage = "password does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
